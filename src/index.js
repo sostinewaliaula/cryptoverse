b16851b3ae8025b0209d75652fdf3c6dce5ce708
sostinewaliaula <83455882+sostinewaliaula@users.noreply.github.com>
2022-03-15 00:14:43 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+    <Router>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </Router>,
+document.getElementById('root')
+);
